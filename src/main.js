31c693edd118ee0d0261df3d27c41cb2c0d20048
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-07-23 14:27:17 +0300

    HW-JS-11

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const input = form.querySelector('input[name="search-text"]');
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const query = input.value.trim();
+  if (!query) {
+    iziToast.warning({
+      message: 'Please enter a search term',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+
+    if (data.hits.length === 0) {
+      iziToast.info({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+    } else {
+      createGallery(data.hits);
+    }
+  } catch (error) {
+    iziToast.error({
+      message: 'An error occurred. Please try again later.',
+      position: 'topRight',
+    });
+  } finally {
+    hideLoader();
+  }
+});
